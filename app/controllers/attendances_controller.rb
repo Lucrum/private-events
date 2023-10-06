0074cf4b823bbf0ad56c0551405f6c213c1e26f7
Elton <28992495+Lucrum@users.noreply.github.com>
2023-10-06 14:35:37 -0400

    More consistency, current and past events

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,10 +1,6 @@
 class AttendancesController < ApplicationController
   before_action :authenticate_user!
 
-  def index
-    @attendances = Attendance.where(user_id: current_user.id)
-  end
-
   def destroy
     @attendance = Attendance.find(params[:id])
     @attendance.destroy
@@ -16,7 +12,7 @@ class AttendancesController < ApplicationController
     event = Event.find(params[:attendance_id])
 
     # ensure user doesn't already have a ticket
-    return if event.attendee.include?(current_user)
+    render 'events/index', status: :unprocessable_entity if event.attendee.include?(current_user)
 
     @ticket = Attendance.new
     @ticket.event = event
