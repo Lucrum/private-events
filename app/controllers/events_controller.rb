d19e4b9e75ff510bf793584bcd60a6dda10a1ae2
Elton <28992495+Lucrum@users.noreply.github.com>
2023-10-06 17:09:56 -0400

    Event editing and destruction

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,10 +9,25 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
   end
 
+  def edit
+    @event = Event.find(params[:id])
+    @author_name = @event.creator.name
+  end
+
   def new
     @event = Event.new
     @event.time = Time.now.strftime('%Y-%m-%dT%H:%M')
     @author_name = current_user.name
+  end
+
+  def update
+    @event = Event.find(params[:id])
+
+    if @event.update(event_params)
+      redirect_to @event
+    else
+      render :edit, status: unprocessable_entity
+    end
   end
 
   def create
@@ -23,6 +38,13 @@ class EventsController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+
+    redirect_to users_path
   end
 
   private
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
-  has_many :attendance
+  has_many :attendance, dependent: :destroy
   has_many :attendee, through: :attendance, source: :user
 
   scope :past, -> { where('time < ?', Date.today) }
